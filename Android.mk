29712a5ee259dcec6b8b8cbb10061f1449d879d7
Josip Kelecic <kelecicj@gmail.com>
2026-09-24 10:43:33 +0200

    vontarx4: Switch proprietary blobs to the stock ah218 dump
    Default to Amlogic/ah218. Keep ohm-pinned DRM libs and jarvis-pinned
    Netflix xmls. Drop Amlogic KeyMint blobs and ohm TEE applets that this
    board does not use.

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),vontarx4)
+
+$(call add-radio-file-sha1-checked,radio/bootloader.img,6bcaf7bab5aab0335fe2ed64df56d7028ee22956)
+
+endif
---
+++ vontarx4-vendor.mk
@@ -0,0 +1,26 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/amlogic/vontarx4
+
+PRODUCT_COPY_FILES += \
+    vendor/amlogic/vontarx4/proprietary/product/etc/sysconfig/netflix.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/netflix.xml \
+    vendor/amlogic/vontarx4/proprietary/recovery/root/system/etc/mesondisplay.cfg:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/mesondisplay.cfg \
+    vendor/amlogic/vontarx4/proprietary/vendor/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc \
+    vendor/amlogic/vontarx4/proprietary/vendor/etc/mesondisplay.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mesondisplay.cfg \
+    vendor/amlogic/vontarx4/proprietary/vendor/etc/permissions/droidlogic.software.netflix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/droidlogic.software.netflix.xml \
+    vendor/amlogic/vontarx4/proprietary/vendor/lib/firmware/aucpu_fw.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/aucpu_fw.bin \
+    vendor/amlogic/vontarx4/proprietary/vendor/lib/firmware/video/video_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/video/video_ucode.bin \
+    vendor/amlogic/vontarx4/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta \
+    vendor/amlogic/vontarx4/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/amlogic/vontarx4/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta
+
+PRODUCT_PACKAGES += \
+    libamavutils \
+    libsecmem \
+    libHwAudio_dcvdec \
+    libHwAudio_dtshd \
+    android.hardware.oemlock@1.0-service.droidlogic.xml \
+    android.hardware.oemlock@1.0-service.droidlogic
